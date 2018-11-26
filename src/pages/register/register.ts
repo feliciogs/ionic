00316c82c63c809967f302fe1e5a81aaefac3381
gabriel@poscontrole.com.br <gabriel@poscontrole.com.br>
2018-11-26 18:21:57 -0300

    Update 7

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { Band } from '../../models/band.model';
 import { SongService } from '../../services/song.service';
-
-/**
- * Generated class for the RegisterPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { Observable } from 'rxjs/Observable';
+import { Band } from '../../models/band.model';
+import { Song } from '../../models/song.model';
+import { LoadingController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -16,15 +12,28 @@ import { SongService } from '../../services/song.service';
   templateUrl: 'register.html',
 })
 export class RegisterPage {
+  bandsList$: Observable<Band[]>;
   band: Band={
     name:''
   }
-  constructor(public navCtrl: NavController, public navParams: NavParams, private regBand: SongService) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private regBand: SongService
+    ,private songsService: SongService, public loadingCtrl: LoadingController) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad RegisterPage');
+  ionViewWillLoad() {
+    const loader = this.loadingCtrl.create({
+      content: "Carregando aguarde...",
+      duration: 1500
+    });
+    this.bandsList$ = this.songsService.getBandList().snapshotChanges().map(changes=>{
+      return changes.map(c => ({
+        key: c.payload.key, ...c.payload.val()
+      }));
+    });
+    loader.present();
+    console.log(this.bandsList$);
   }
+  
   addBand(band: Band){
     this.regBand.addBand(band).then(ref=>{
       this.navCtrl.setRoot('HomePage', {key:ref.key});
